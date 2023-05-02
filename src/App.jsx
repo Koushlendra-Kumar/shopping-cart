94d245c42452f085ef386256bcb42668ed801617
Koushlendra Kumar <code.koushlendra@gmail.com>
2023-05-02 16:03:06 +0530

    ✨ Introduce add to cart feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Popover, Button } from '@mui/material';
 
 function App() {
   const [data, setData] = useState([]);
   const [cart, setCart] = useState([]);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
 
   useEffect(() => {
     axios
@@ -11,6 +24,32 @@ function App() {
       .then((res) => setData(res.data))
       .catch((err) => console.log(err));
   }, []);
+
+  useEffect(() =>{
+    console.log(cart)
+  },[cart])
+
+  const cartProducts = cart.length ? cart.map((elem) => {
+    let item = data.find(product => product.id == elem.id)
+    console.log(item);
+    return (
+    <div
+      key={item.id}
+      className="w-sm m-2 bg-red-400"
+      flex="~ justify-center items-center"
+    >
+      <img
+        src={item.image}
+        height="50"
+        width="50"
+        border="~ solid green 1px rounded"
+        className="self-center"
+      />
+      <h6>{item.title.substring(0, 20) +'...'}</h6>
+      <h3>${item.price}</h3>
+      <div className='ml-5'><span>-</span>{elem.quantity}<span>+</span></div>
+    </div>
+  )}) : <p className="m-10">Add items to cart </p>
 
   const products = data.map((item) => (
     <div
@@ -30,7 +69,21 @@ function App() {
       <h3>${item.price}</h3>
       <button
         type="button"
-        onClick={() => setCart([...cart, item.id])}
+        onClick={() => {
+          let itemExist = cart.find(elem => elem.id == item.id);
+          if(itemExist){
+            let newCart = cart.map(elem => {
+              if(elem.id == item.id){
+                return {...elem, quantity: elem.quantity + 1}
+              } else {
+                return elem;
+              }
+            })
+            setCart(newCart);
+          } else {
+            setCart([...cart, {id: item.id, quantity: 1}])
+          }
+        }}
         className="bg-amber h-8 w-28 hover:bg-blue"
       >
         Add to cart
@@ -43,9 +96,21 @@ function App() {
       <header flex="~ justify-around items-center">
         <h1>Shopping Cart</h1>
         <div className="h-18 w-md" flex="~ justify-around items-center">
-          <a href="#" h-full flex="~ justify-center items-center">
-            Cart({cart.length})
-          </a>
+        <Button aria-describedby={id} variant="contained" onClick={handleClick}>
+        Open Popover
+        </Button>
+        <Popover
+          id={id}
+          open={open}
+          anchorEl={anchorEl}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+          }}
+        >
+          {cartProducts}
+        </Popover>
           <a href="#" className="flex flex-justify-around flex-items-center">
             Orders
           </a>
