b18c230d81b01c9114aba457df02b301bf751709
Web Dev Koushal <95288059+Koushlendra-Kumar@users.noreply.github.com>
2023-07-06 04:01:50 +0000

    :wrench: Remove unocss configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
 import { defineConfig } from "vite";
 import React from "@vitejs/plugin-react";
-import UnoCSS from "unocss/vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    UnoCSS({
-      configFile: '/uno.config.ts'
-    }), 
     React()
   ],
 });
