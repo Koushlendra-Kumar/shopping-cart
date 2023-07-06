15d647b3a203299061ca4132a54dd213c6f3627e
Web Dev Koushal <95288059+Koushlendra-Kumar@users.noreply.github.com>
2023-07-06 04:02:54 +0000

    :lipstick: Add material ui theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import 'virtual:uno.css'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ThemeProvider } from "@emotion/react";
+import { CssBaseline, createTheme } from "@mui/material";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#42a5f5",
+    },
+    secondary: {
+      main: "#f3e5f5",
+    },
+    error: {
+      main: "#d32f2f",
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
